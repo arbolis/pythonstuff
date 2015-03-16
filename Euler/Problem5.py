ed78ac9e8bc32f4958a8f3d6ce99f90fed5d0a8e
arbolis <anjeeisaac@gmail.com>
2015-03-16 02:14:32 -0300

    I solved it but takes about 10 mins to run

--- Euler/Problem5.py
+++ Euler/Problem5.py
@@ -1,5 +1,5 @@
 import itertools
-for candidate in itertools.count(11):
-    if all(candidate % i == 0 for i in range(1, 21)):
+for candidate in itertools.count(12252240):
+    if all(candidate % i == 0 for i in range(1, 20)):
         print(candidate)
         break
