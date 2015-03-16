8eeefcd389b78ed9e080f508c405a7b11ccbae23
arbolis <anjeeisaac@gmail.com>
2015-03-16 08:13:31 -0300

    Nice optimization without hack!

--- Euler/Problem5.py
+++ Euler/Problem5.py
@@ -1,5 +1,5 @@
 import itertools
-for candidate in itertools.count(232752240):
+for candidate in itertools.count(20, 20):
     if all(candidate % i == 0 for i in range(1, 21)):
         print(candidate)
         break
