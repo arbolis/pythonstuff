5094b214f566d9bc20cb93fa537f285ca162eb37
arbolis <anjeeisaac@gmail.com>
2015-03-16 08:26:17 -0300

    Optimization but still takes too much time

--- Euler/Problem142.py
+++ Euler/Problem142.py
@@ -1,9 +1,10 @@
 squares = []
 for i in range(1, 2000):
     squares.append(i**2)
-for x in range(1, 1000):
-    for y in range(1, 1000):
-        for z in range(1, 1000):
+# Now I choose x>y>z
+for x in range(3, 1000):
+    for y in range(2, x):
+        for z in range(1, y):
             a = x+y
             b = x-y
             c = x+z
@@ -12,6 +13,7 @@ for x in range(1, 1000):
             f = y-z
             if (a in squares and b in squares and c in squares and
                 d in squares and e in squares and f in squares):
-                print(x)
-                print(y)
-                print(z)
+                print("x=", x)
+                print("y=", y)
+                print("z=", z)
+                print("x+y+z=", x+y+z)
