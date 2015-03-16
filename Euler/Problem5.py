3845fd0a2478ea591a9b1bfb3e33948470d9f7c4
arbolis <anjeeisaac@gmail.com>
2015-03-16 08:15:05 -0300

    Nice optimization without hack!

--- Euler/Problem5.py
+++ Euler/Problem5.py
@@ -1,4 +1,6 @@
 import itertools
+# I start with candidate=20 and I increement by steps of 20. Nice trick
+# and no hack, that is, the seed is 20 and not close to the solution.
 for candidate in itertools.count(20, 20):
     if all(candidate % i == 0 for i in range(1, 21)):
         print(candidate)
