518510209011ba974c48c0da405bb8cb53dbfd08
arbolis <anjeeisaac@gmail.com>
2015-03-16 02:25:13 -0300

    Version that runs fast because already close to the result

--- Euler/Problem5.py
+++ Euler/Problem5.py
@@ -1,5 +1,9 @@
 import itertools
-for candidate in itertools.count(12252240):
-    if all(candidate % i == 0 for i in range(1, 20)):
+for candidate in itertools.count(232752240):
+    if all(candidate % i == 0 for i in range(1, 21)):
         print(candidate)
         break
+
+
+# This version starts very close to the result I previously got and
+# so runs much faster
